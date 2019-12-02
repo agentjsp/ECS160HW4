285885594ae7725480fe11c8c3a90a7b0ec51004
SunGoesRowr <heplin@ucdavis.edu>
2019-12-01 21:24:00 -0800

    Did some refactoring + add boolean for quotes
    Took out a block of code and made it into a function. Took processing the header file from the main loop and put it into a if statement before it.
    
    Added a global boolean for telling if we have quotes or not.

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,34 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "stdbool.h"
 
 #define ROW_LENGTH 1024
 #define MAX_TWEETS 20000
+
+size_t tweeter_col = 99;
+bool has_quotes;
 
 typedef struct tweeter{
 	char *name;
 	int count;
 } tweeter;
 
-void add_name(tweeter *list, int list_size, char *name)
+tweeter list[MAX_TWEETS];
+
+int add_name(size_t* list_size, char* name)
 {
-	for(int i = 0; i < list_size; i++){
+	printf("add_name: list size is currently %ld\n", *list_size);
+	for(int i = 0; i < *list_size; i++){
 		// They are the same name
+		printf("add_name: comparing names %s and %s at %d \n", list[i].name, name, i);
 		if(strcmp(list[i].name, name) == 0){
+			printf("add_name: name match\n");
 			list[i].count++;
-			return;
+			return 1;
 		}
 	}
 
-	if(list_size < MAX_TWEETS){
-		list[list_size + 1].name = name;
-		list[list_size + 1].count = 0;
-	}
-	else{
+	if(*list_size < MAX_TWEETS){
+		printf("add_name: adding list entry %s at %ld\n", name, *list_size);
+		list[*list_size].name = malloc(sizeof(char) * strlen(name));
+		strcpy(list[*list_size].name, name);
+		list[*list_size].count = 1;
+		(*list_size)++;
+	} else {
 		printf("Invalid input format\n");
 		exit(1);
-	}	
-}	
+	}
+	return 0;
+}
+
+void process_header(char *row_cpy)
+{
+	if(row_cpy == NULL){
+		printf("No header\n");
+		exit(1);
+	}
+
+	int name_occurence = 0;
+	int curr_col = 0;
+	char* token;
+	while ((token = strsep(&row_cpy, ","))) {
+        	if (strcmp(token, "name") == 0) {
+			tweeter_col = curr_col;
+			name_occurence++;
+			has_quotes = false;
+		}
+
+		if(strcmp(token, "\"name\"") == 0){
+			tweeter_col = curr_col;
+			name_occurence++;
+			has_quotes = true;
+		}
+		curr_col++;
+	}
+
+	if(name_occurence > 1){
+		printf("Invalid input format\n");
+		exit(1);
+	}
+
+	return;
+}
 
 int main(int argc, char** argv) {
 	if (argc < 1) {
@@ -43,15 +88,46 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	size_t row_len = 1024;
+	size_t row_len = 1024, curr_row = 0, list_size = 0;
 	char* row = malloc(row_len);
+	
+	// Process the header to get tweeter column
+	if(getline(&row, &row_len, fp) > -1){
+		char* row_cpy = row;
+		process_header(row_cpy);
+		
+	}
+	else{
+		printf("Couldn't get header\n");
+		exit(1);
+	}
 
 	//parse csv file line by line
 	while (getline(&row, &row_len, fp) > -1) {
-		printf("%s", row);
+		printf("Current row: %s\n", row);
+		char* row_cpy;
+		char* token;
+		size_t curr_col = 0;
+
+		row_cpy = row;
+
+		//parse line token by token
+		while ((token = strsep(&row_cpy, ","))) {
+			printf("Current token: %s \n", token);
+			if (curr_col == tweeter_col) {
+				printf("launching add_name\n");
+				add_name(&list_size, token);
+			}
+			curr_col++;
+		}
+		curr_row++;
 	}
 
 	free(row);
+
+	for(int i = 0; i < list_size; i++){
+		printf("name: %s count: %d\n", list[i].name, list[i].count);
+	}
 
 	return 0;
 }
