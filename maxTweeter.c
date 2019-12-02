f9829221f4da27896e6a0b38835387d45ab9b284
SunGoesRowr <heplin@ucdavis.edu>
2019-12-02 10:24:53 -0800

    Added sorting and printing tweeter array
    As description said. Also commented out unneeded print statements. Will clean up later.

--- maxTweeter.c
+++ maxTweeter.c
@@ -18,34 +18,72 @@ tweeter list[MAX_TWEETS];
 
 int add_name(size_t* list_size, char* name)
 {
-	printf("add_name: list size is currently %ld\n", *list_size);
+	//printf("add_name: list size is currently %ld\n", *list_size);
 	for(int i = 0; i < *list_size; i++){
 		// They are the same name
-		printf("add_name: comparing names %s and %s at %d \n", list[i].name, name, i);
+		//printf("add_name: comparing names %s and %s at %d \n", list[i].name, name, i);
 		if(strcmp(list[i].name, name) == 0){
-			printf("add_name: name match\n");
+			//printf("add_name: name match\n");
 			list[i].count++;
 			return 1;
 		}
 	}
 
 	if(*list_size < MAX_TWEETS){
-		printf("add_name: adding list entry %s at %ld\n", name, *list_size);
+		//printf("add_name: adding list entry %s at %ld\n", name, *list_size);
 		list[*list_size].name = malloc(sizeof(char) * strlen(name));
 		strcpy(list[*list_size].name, name);
 		list[*list_size].count = 1;
 		(*list_size)++;
 	} else {
-		printf("Invalid input format\n");
+		printf("Invalid Input Format\n");
 		exit(1);
 	}
 	return 0;
 }
 
-void process_header(char *row_cpy)
+char* unquote(char* name)
+{	
+	int str_len = strlen(name);
+
+	if(name == NULL){
+		printf("Invalid Input Format\n");
+		exit(1);
+	}
+
+	// Make sure it ends in quotes if it should	
+	if(has_quotes){
+		if((name[0] != '\"') || (name[str_len - 1] != '\"')){
+                	printf("Invalid Input Format\n");
+                	exit(1);
+        	}
+	}
+
+	if(!has_quotes){
+		if((name[0] == '\"') && (name[str_len - 1] == '\"')){
+                	printf("Invalid Input Format\n");
+                	exit(1);
+        	}
+		return name;
+	}
+
+
+	// Case where name is just "" and it has quotes
+	if(str_len - 2 <= 0){
+		printf("Invalid Input Format\n");
+		exit(1);
+	}
+
+	// Get the name between the quotes
+	strtok(name, "\"");
+
+	return strtok(name, "\"");
+}
+
+int process_header(char *row_cpy)
 {
 	if(row_cpy == NULL){
-		printf("No header\n");
+		printf("Invalid Input Format\n");
 		exit(1);
 	}
 
@@ -68,10 +106,39 @@ void process_header(char *row_cpy)
 	}
 
 	if(name_occurence > 1){
-		printf("Invalid input format\n");
+		printf("Invalid Input Format\n");
 		exit(1);
 	}
 
+	return curr_col;
+}
+
+void tweeter_sort(int list_size)
+{
+	for (int i = 0; i < list_size; i++){                    
+		for (int j = 0; j < list_size; j++){             
+			if (list[j].count < list[i].count){               
+				tweeter tmp = list[i];         
+				list[i] = list[j];           
+				list[j] = tmp;            
+			}
+		}
+	}
+}
+
+void tweeter_print(int list_size)
+{
+	int counter;
+	if(list_size < 10){
+		counter = list_size;
+	}
+	else{
+		counter = 10;
+	}
+
+	for(int i = 0; i < counter; i++){
+		printf("%s: %d\n", list[i].name, list[i].count);
+	}
 	return;
 }
 
@@ -90,21 +157,30 @@ int main(int argc, char** argv) {
 
 	size_t row_len = 1024, curr_row = 0, list_size = 0;
 	char* row = malloc(row_len);
+	int total_col = 0;
+	bool empty_line = false;
 	
 	// Process the header to get tweeter column
 	if(getline(&row, &row_len, fp) > -1){
 		char* row_cpy = row;
-		process_header(row_cpy);
+		total_col = process_header(row_cpy);
 		
 	}
 	else{
-		printf("Couldn't get header\n");
+		printf("Invalid Input Format\n");
 		exit(1);
 	}
 
 	//parse csv file line by line
 	while (getline(&row, &row_len, fp) > -1) {
-		printf("Current row: %s\n", row);
+		
+		//printf("Current row: %s\n", row);
+		// empty line found
+		if(row[strspn(row, " \n")] == '\0'){
+			empty_line = true;
+			continue;
+		}
+
 		char* row_cpy;
 		char* token;
 		size_t curr_col = 0;
@@ -113,29 +189,33 @@ int main(int argc, char** argv) {
 
 		//parse line token by token
 		while ((token = strsep(&row_cpy, ","))) {
-			printf("Current token: %s \n", token);
-			if (curr_col == tweeter_col) {
-				printf("launching add_name\n");
-				//if header is quoted, check for valid
-				//quote configurations on column entries
-				if (has_quotes) {
-					if ((token[0] != '"') || (token[strlen(token)-1] != '"')) {
-						printf("Invalid input format\n");
-						return 1;
-					}
-				}
-				add_name(&list_size, token);
+			//printf("Current token: %s \n", token);
+				
+			if (curr_col == tweeter_col){
+				//printf("launching add_name\n");
+				add_name(&list_size, unquote(token));
 			}
 			curr_col++;
 		}
 		curr_row++;
+		// count of parsed field doesn't match header count
+		if(curr_col != total_col){
+			printf("Invalid Input Format\n");
+			exit(1);
+		}
+
+		// found more content after finding empty line
+		if(empty_line){
+			printf("Invalid Input Format\n");
+                	exit(1);
+		}
 	}
 
 	free(row);
 
-	for(int i = 0; i < list_size; i++){
-		printf("name: %s count: %d\n", list[i].name, list[i].count);
-	}
+	tweeter_sort(list_size);
+
+	tweeter_print(list_size);
 
 	return 0;
 }
