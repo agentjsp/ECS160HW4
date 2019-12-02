38e411030a72ed3ae2db5429fd3d2d8b20c784ca
SunGoesRowr <heplin@ucdavis.edu>
2019-12-01 17:12:15 -0800

    Added higher level ideas
    Added a tweeter struct type. And a way to store and update critical info.

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,6 +1,34 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ROW_LENGTH 1024
+#define MAX_TWEETS 20000
+
+typedef struct tweeter{
+	char *name;
+	int count;
+} tweeter;
+
+void add_name(tweeter *list, int list_size, char *name)
+{
+	for(int i = 0; i < list_size; i++){
+		// They are the same name
+		if(strcmp(list[i].name, name) == 0){
+			list[i].count++;
+			return;
+		}
+	}
+
+	if(list_size < MAX_TWEETS){
+		list[list_size + 1].name = name;
+		list[list_size + 1].count = 0;
+	}
+	else{
+		printf("Invalid input format\n");
+		exit(1);
+	}	
+}	
 
 int main(int argc, char** argv) {
 	if (argc < 1) {
